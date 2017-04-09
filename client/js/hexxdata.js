2efd47b0e45533c97a24c9e5bcdb8ec32a89e174
James Echmalian <ech@ech.net>
2017-04-09 10:00:59 -0400

    Clean up

---
+++ client/js/hexxdata.js
@@ -0,0 +1,51 @@
+
+HEXX.config = {
+	palette: {
+		elementRadius: 30,
+		lineStyle: "rgba(150,150,150,0.5)",
+		lineWidth: 2
+	},
+	canvas: {
+		width: 520,
+		height: 420, 
+		elementRadius: 30,
+		lineStyle: "rgba(150,150,150,0.5)",
+		lineWidth: 2
+	}
+};
+
+HEXX.data = {
+	palette: {
+		id: 0,
+		name: "palette0",
+		contents: [
+			{
+				id: 1,
+				name: "castle",
+				image: {
+					url: "img/castle.jpg"
+				}
+			},
+			{
+				id: 2,
+				name: "red",
+				fill: "rgba(255,0,0,1)"
+			},
+			{
+				id: 3,
+				name: "blue",
+				fill: "rgba(0,0,255,1)"
+			},
+			{
+				id: 4,
+				name: "green",
+				fill: "rgba(0,255,0,1)"
+			}
+		]
+	},
+	canvas: {
+		id: 47,
+		name: "rich",
+		contents: []
+	}
+};
